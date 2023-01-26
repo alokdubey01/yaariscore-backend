f9e134f5b55108270edb3783b61babf7686bb6cc
Alok Dubey <alok73327@gmail.com>
2023-01-27 00:14:20 +0530

    new games

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use('/2048',express.static("public/2048"));
 app.use('/car',express.static("public/highway-race/dist"));
 app.use('/tower',express.static("public/tower"));
 app.use('/maze',express.static("public/pickle_rick"));
-app.use('/pong',express.static("public/pong"));
 app.use('/red-ball',express.static("public/platform-game-engine"));
 app.use('/memoryIt',express.static("public/Dinasour"));
 app.use('/tile-block',express.static("public/tile-block/dist"));
 app.use('/astray',express.static("public/Astray"));
+app.use('/aviator',express.static("public/TheAviator"));
 
 app.get("/", (req, res) => {
   res.send("Hii i am working fine");
@@ -40,23 +40,15 @@ app.get("/maze", (req, res) => {
   res.sendFile(path.join(__dirname, "public/pickle_rick/app.html"));
 });
 
-app.get("/pong", (req, res) => {
-    // there a issue with this game
-  res.sendFile(path.join(__dirname, "public/pong/app.html"));
-});
-
 app.get("/tetris", (req, res) => {
-    // there a issue with this game
   res.sendFile(path.join(__dirname, "public/tetris/app.html"));
 });
 
 app.get("/memoryIt", (req, res) => {
-    // there a issue with this game
   res.sendFile(path.join(__dirname, "public/Dinasour/app.html"));
 });
 
 app.get("/tile-block", (req, res) => {
-    // there a issue with this game
   res.sendFile(path.join(__dirname, "public/tile-game/dist/index.html"));
 });
 
@@ -66,8 +58,11 @@ app.get("/red-ball", (req, res) => {
 });
 
 app.get("/astray", (req, res) => {
-    // there a issue with this game
   res.sendFile(path.join(__dirname, "public/Astray/index.html"));
+});
+
+app.get("/aviator", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/TheAviator/index.html"));
 });
 
 app.listen(port, () => {
