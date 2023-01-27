742bacfc7386d2497f2285a249a62f0f9ffea880
Alok Dubey <alok73327@gmail.com>
2023-01-27 13:23:46 +0530

    testing

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require('cors');
 const port = process.env.PORT || 8000
 app.use(cors())
 
-app.use('/cube',express.static("public/cube"));
-app.use('/2048',express.static("public/2048"));
-app.use('/car',express.static("public/highway-race/dist"));
-app.use('/tower',express.static("public/tower"));
-app.use('/maze',express.static("public/pickle_rick"));
+// app.use('/cube',express.static("public/cube"));
+// app.use('/2048',express.static("public/2048"));
+// app.use('/car',express.static("public/highway-race/dist"));
+// app.use('/tower',express.static("public/tower"));
+// app.use('/maze',express.static("public/pickle_rick"));
 app.use('/red-ball',express.static("public/platform-game-engine"));
 app.use('/memoryIt',express.static("public/Dinasour"));
 app.use('/tile-block',express.static("public/tile-block/dist"));
@@ -20,25 +20,25 @@ app.get("/", (req, res) => {
   res.send("Hii i am working fine");
 });
 
-app.get("/cube", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/cube/app.html"));
-});
+// app.get("/cube", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/cube/app.html"));
+// });
 
-app.get("/2048", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/2048/app.html"));
-});
+// app.get("/2048", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/2048/app.html"));
+// });
 
-app.get("/car", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/highway-race/dist/index.html"));
-});
+// app.get("/car", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/highway-race/dist/index.html"));
+// });
 
-app.get("/tower", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/tower/app.html"));
-});
+// app.get("/tower", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/tower/app.html"));
+// });
 
-app.get("/maze", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/pickle_rick/app.html"));
-});
+// app.get("/maze", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/pickle_rick/app.html"));
+// });
 
 app.get("/tetris", (req, res) => {
   res.sendFile(path.join(__dirname, "public/tetris/app.html"));
